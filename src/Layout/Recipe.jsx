fb31ce6e4ed94aec678e453cd3d603ddadcdedd2
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2025-01-04 15:26:21 +0600

    recipes is done

--- src/Layout/Recipe.jsx
+++ src/Layout/Recipe.jsx
@@ -1,5 +1,13 @@
 const Recipe = () => {
-  return <div>Recipe</div>;
+  return (
+    <div className="text-center">
+      <h2 className="text-4xl font-semibold">Our Recipes</h2>
+      <p className="text-lg opacity-60 w-2/3 mx-auto mt-3">
+        Our Food is the best food all over the city. So, you should test and
+        supporting with others. Our Food is the best food all over the city.
+      </p>
+    </div>
+  );
 };
 
 export default Recipe;
